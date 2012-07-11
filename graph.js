a8ddefc473e6c6e18cf4a6e30a37ddbea91067d1
Nathan Oehlman <nathan.oehlman@sidelab.com>
2012-07-11 14:32:46 +1000

    Added graph.js for npm

---
+++ graph.js
@@ -0,0 +1,9 @@
+var graph = require('./lib/graph'),
+    edge = require('./lib/edge'),
+    vertex = require('./lib/vertex');\
+
+module.exports = {
+    Graph: graph.Graph,
+    Edge: edge.Edge,
+    Vertex: vertex.Vertex
+}
